c84af838fe288b06d0474cf8c98372b5ad80bb15
rakshithreddysiram <siramrakshithreddy@gmail.com>
2025-11-06 15:54:56 +0530

    Initial commit for GitHub Pages hosting

--- backend/routes/trackingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getOrderTracking } = require('../controllers/trackingController');
-
-// Public route for order tracking
-router.route('/:id').get(getOrderTracking);
-
-module.exports = router;
